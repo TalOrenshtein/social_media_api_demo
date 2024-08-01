cc14f9ff8b0edabc9e697589393c263ececc8539
Tal <tal.orenshtein1@gmail.com>
2024-08-01 22:04:13 +0300

    * fixed a bug in handle_expand * encapsulate and generalize some logic * removed unused functions * changed expand_response_helper implementation to be based on pydantic models instead of SQL tables

--- api/v1/utils.py
+++ api/v1/utils.py
@@ -61,87 +61,32 @@ def get_sql_schema(table:str)->dict:
                 return schema
         raise ValueError(f'{table} table not found.')
 
-def is_sorted_by_char(arr:list,char:str):
+def get_depenable_model(metadata)->list:
     r'''
-    check if the elements in arr are sorted by the number of appearences of char.
-    :param list arr: a list with str values
-    :param str char: an ABC character.
-    :returns: results that indicates if arr is sorted or not.
+    searching a pydantic model's metadata for dependentable model.
+    :param metadata: A pydantic model's metadata (from model.model_fields)
+    :returns: A list that list al the dependable fields, depending if dependable_fields is True/False.
     '''
-    if len(char)!=1:
-        raise ValueError('char arg is not a char.')
-    count_arr=[]
-    for i in range(len(arr)):
-        if not isinstance(arr[i],str):
-            raise ValueError("arr's arg elements are not of type 'str'")
-        count_arr.insert(i,arr[i].count(char))
-    for i in range(1,len(count_arr)):
-        if count_arr[i-1]>count_arr[i]:
-            return False
-    return True
+    args=[]
+    if get_origin(metadata.annotation) is Union:
+            for arg in get_args(metadata.annotation):
+                if get_origin(arg) is Annotated and type(get_args(arg)[1]) is SerializeAsAny: # assuming all polymorphism was enabled via using SerializeAsAny
+                    args.append(get_args(arg)[0])
+    elif issubclass(metadata.annotation,BaseModel):
+        args.append(metadata.annotation)
+    return args
 
-def dest_resp_formatter(data:dict,dest:dict,sep:str):
-    r'''
-    format the response of a specific dest calculated by expand_response function.
-    :param dict dest: A dict that includes the following key (type) value pairs:
-        * type: (str): The destation which was requests. dest might be nested.
-        * id (dict): The src's object id. If dest is nested, it's the id of the first object
-    :param dict data: expand_response result
-    :param str sep: A seperator character
-    :returns: A dict that the caller can just replace with his original one.
-    '''
-    print("dest_frmtr")
-    return dest_resp_formatter_helper(data,dest,sep)
-
-def dest_resp_formatter_helper(data:dict,dest:dict,sep:str,d:dict={}):
-    print(f'params:{data}\n{dest}]\n{d}\nend-params')
-    if dest['type'].count(sep)==0:
-        print(" here depth left 0")
-        d[dest['type']]=data[f"{dest['type']}@{dest['id']}"]
-        return d
-    dest_splitted=dest['type'].split(sep)
-    print(dest_splitted)
-    print (f'here depth left {len(dest_splitted)-1}')
-    print(f"1  -  {d}")
-    d[dest_splitted[0]]=data[f"{dest_splitted[0]}@{dest['id']}"]
-    print(f"2  -  {d}")
-    d[dest_splitted[0]][dest_splitted[1]]=dest_resp_formatter_helper(
-        data,
-        {'type':dest['type'][dest['type'].index(dest_splitted[1]):],
-        'id':d[dest_splitted[0]][dest_splitted[1]]},
-        sep,
-        d
-        ) #this element currently hold the id, so we replace the id with the whole object, and use said id for the swap.
-    print(f"3  -  {d}")
-    return d
-    
-def expand_response_formatter(src:str,destList:list,res:dict)->dict:
-    r'''
-    format the response calculated by expand_response function.
-    :param str src: The type of the source object that requested the expandation
-    :param list destList: A list of dicts, sorted by the amount of dots (.), that includes the following key (type) value pairs:
-        * type: (str): The destation which was requests. dest might be nested.
-        * id (dict): The src's object id. If dest is nested, it's the id of the first object
-    :param res:  expand_response result
-    :returns: A dict that the caller can just replace with his original one.
-    '''
-    if not is_sorted_by_char([e['type'] for e in destList],'.'): raise ValueError("destList param not sorted by '.'")
-    the_final_dict={}
-    for dest in destList:
-        the_final_dict[dest['type'].split('.',1)[0]]=dest_resp_formatter(res,dest,'.')
-    return the_final_dict
-
-def expand_response(src:str,destList:list)->dict|None:
+def expand_response(src:str,destList:list,src_model:BaseModel)->dict|None:
     r'''
     get the detail of the requested dests until depth of env.EXPAND_MAX_DEPTH nested dests
     :param str src: The type of the source object that requested the expandation
     :param list destList: A list of dicts that includes the following key (type) value pairs:
         * type: (str): The destation which was requests. dest might be nested.
         * id (dict): The src's object id. If dest is nested, it's the id of the first object
+    :param  BaseModel src_model: The source's response model
     :returns: A dict with dest's details in it, or none if not found.
     '''
     destList.sort(key=lambda e:e['type'].count("."))
-    destList_copy=deepcopy(destList)
     srcHolder=src #saving the original source to use for each elemenet in destList
     res={}
     memo={} #using memoraztion technique to avoid unnecessary helper calls when possible, to try and reduce load on the db. It seems reasonable to memo as the user probably won't expand too many unique fields, and in general it still keeps the space complexity at O(n).
@@ -160,11 +105,11 @@ def expand_response(src:str,destList:list)->dict|None:
                     raise ValueError(f"field '{dests[0]}' isn't expandable.")
             if not memo:
                 memo={}
-            if not f'{dests[0]}@{dest['id']}' in memo:
-                # print(f'in if: {dests[0]}@{dest['id']}. src:{src}')
+            if f'{dests[0]}@{dest['id']}' not in memo:
                 dest_object=expand_response_helper(
                     src,
-                    {"type":dests[0],"id":dest['id']}
+                    {"type":dests[0],"id":dest['id']},
+                    src_model
                 )
                 memo[f'{dests[0]}@{dest['id']}']=dest_object
                 res_traveler[dests[0]]=memo[f'{dests[0]}@{dest['id']}']
@@ -174,31 +119,34 @@ def expand_response(src:str,destList:list)->dict|None:
                 res_traveler[dests[0]]=memo[f'{dests[0]}@{dest['id']}']
                 if len(dests)>1:
                     dest['id']=memo[f'{dests[0]}@{dest['id']}'][dests[1]]
-                # print(f"in else:src={src}, id={dest['id']}")
-            #if isinstance(res_traveler[dests[0]],dict):
             res_traveler=res_traveler[dests[0]]
             src=dests[0]
             dests.pop(0)
             
             dest['type']=".".join(dests)
     return res
-    #return expand_response_formatter(src,destList_copy,memo)
 
-def expand_response_helper(src:str,dest:dict)->dict|None:
+def expand_response_helper(src:str,dest:dict,src_model,sql_instead_of_pydantic:bool=False)->dict|None:
     r'''
     get the dest object of the requested dest with depth of 1
     :param str src: The type of the source object that requested the expandation
     :param dict dest: A dict that includes the following key (type) value pairs:
         * type: (str): The destation which was requests. dest might be nested.
         * id (dict): The src's object id. If dest is nested, it's the id of the first object
+    :param  BaseModel src_model: The source's response model
+    :param boo sql_instead_of_pydantic: will judge if dest['type'] is related to src using SQL table column names instead of pydantic models (not sure myself which is better to use)
     :returns: A dict with dest's details in it, or none if not found.
     '''
     #validating that dest is related to src.
-    try: #TODO: If you can do it with schemas instead, it'd be great.
-        related=dest['type'] in get_sql_schema(f'{src}s')
+    independable_fields=[]
+    for field,meta_data in src_model.model_fields.items():
+        if meta_data.description is None: #by convention, the field's description will hold the name of the external object that said field depend on, represented as a single resource.
+            independable_fields.append(field)
+    try:
+        related=dest['type'] in get_sql_schema(f'{src}s') if  sql_instead_of_pydantic else dest['type'] in independable_fields
     except ValueError:
         try:
-            related=dest['type'] in get_sql_schema(f'{src}')
+            related=dest['type'] in get_sql_schema(f'{src}') if  sql_instead_of_pydantic else dest['type'] in independable_fields
         except _:
             raise ValueError(f"{dest['type']} isn't related to {src}")
     if not related:
@@ -216,26 +164,25 @@ def expand_response_helper(src:str,dest:dict)->dict|None:
         ''',[dest['id']]) #TODO: find more elegant solution to this ugly {dest['type']}s
         return cur.fetchone()
 
-def handle_model_fields_dependencies(data:dict,model:BaseModel)->dict:
+def handle_model_fields_dependencies(data:dict,response_model:BaseModel)->dict:
     r'''
-    Getting a dict that's ready for unpacking to pydantic model while getting values for fields that depends on an external object
+    Making a dict that's ready for unpacking to pydantic model while getting values for fields that depends on an external object
     :param dict data: A data for a pydentic model which is not necessary ready to be unpacked due to unhandled fields that're dependent of an external object.
-    :param: BaseModel model: A pydentic model template which the data will be ready to unpack to.
+    :param: BaseModel response_model: A pydentic response model schema which the data will be based on.
     :returns: a ready to unpack dict.
     '''
     object_fields_mapping={} #holds object names as keys and list of values as value.
-    for field,meta_data in model.model_fields.items():
+    for field,meta_data in response_model.model_fields.items():
         if meta_data.description is not None:
             external_object=meta_data.description #by convention, the field's description will hold the name of the external object that said field depend on, represented as a single resource.
             if external_object not in object_fields_mapping:
                 object_fields_mapping[external_object]=[field]
             else:
                 object_fields_mapping[external_object].append(field)
-        #create model recursively (nested models might have dependent fields in them as well)
-        elif get_origin(meta_data.annotation) is Union:
-            for arg in get_args(meta_data.annotation):
-                if get_origin(arg) is Annotated and type(get_args(arg)[1]) is SerializeAsAny: # assuming all polymorphism was enabled via using SerializeAsAny
-                    data[field]=handle_model_fields_dependencies(data[field],get_args(arg)[0])
+        else:
+            #create model recursively (nested models might have dependent fields in them as well)
+            for model in get_depenable_model(meta_data):
+                data[field]=handle_model_fields_dependencies(data[field],model)
     with sqlite3.connect('social_media_api.db', check_same_thread=False) as db:
         #set up cursor
         cur=db.cursor()
@@ -255,7 +202,7 @@ def handle_model_fields_dependencies(data:dict,model:BaseModel)->dict:
                 data[key]=value
     return data
 
-def handle_expand(expand:str,obj_data:dict,obj_model:BaseModel,obj_title_collection_resource):
+def handle_expand(expand:str,obj_data:dict,obj_response_model:BaseModel,obj_title_collection_resource:str):
     if expand:
         destList=[]
         for e in expand:
@@ -263,16 +210,15 @@ def handle_expand(expand:str,obj_data:dict,obj_model:BaseModel,obj_title_collect
             firstDest=e[:nested_field_dot] if nested_field_dot>-1 else e
             destList.append({'type':f"{e}","id":obj_data[f'{firstDest}']})
         try:
-            expandation=expand_response(obj_title_collection_resource,destList)
+            expandation=expand_response(obj_title_collection_resource,destList,obj_response_model)
         except ValueError as e:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=str(e))
         #unpacking while doing a union with the sql query for fields that wasn't expanded.
         schema_convertion_dict={}
-        for field,meta_data in obj_model.model_fields.items():
-            if get_origin(meta_data.annotation) is Union:
-                for arg in get_args(meta_data.annotation):
-                    if get_origin(arg) is Annotated and type(get_args(arg)[1]) is SerializeAsAny: # assuming all polymorphism was enabled via using SerializeAsAny
-                        schema_convertion_dict[field]=handle_model_fields_dependencies(expandation[field],get_args(arg)[0])
+        for field,meta_data in obj_response_model.model_fields.items():
+            if field in expandation:
+                for model in get_depenable_model(meta_data):
+                    schema_convertion_dict[field]=handle_model_fields_dependencies(expandation[field],model)
             else:
                 schema_convertion_dict[field]=obj_data[field]
-        return handle_model_fields_dependencies(schema_convertion_dict,obj_model)
+        return schema_convertion_dict
